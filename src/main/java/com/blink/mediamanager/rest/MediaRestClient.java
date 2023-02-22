af6c802c347be671a910ea3c399c31c3922610c7
t3rry327 <t3rry327@gmail.com>
2023-02-22 17:53:27 -0300

    gg

--- src/main/java/com/blink/mediamanager/rest/MediaRestClient.java
+++ src/main/java/com/blink/mediamanager/rest/MediaRestClient.java
@@ -1,10 +1,13 @@
 package com.blink.mediamanager.rest;
 
-import java.io.InputStream;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 
+import org.json.JSONObject;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
@@ -14,80 +17,94 @@ import com.blink.mediamanager.MediaError;
 import com.blink.mediamanager.MediaException;
 
 public class MediaRestClient implements MediaTemplate {
-	private RestTemplate rest;
-	private String path;
+    private RestTemplate rest;
+    private String path;
 
 
-	public MediaRestClient() {
-		rest = new RestTemplate();
-	}
+    public MediaRestClient() {
+        rest = new RestTemplate(Arrays.asList(new ResourceHttpMessageConverter()));
+    }
 
-	public MediaRestClient(RestTemplate rest) {
-		this.rest = rest;
-	}
+    public MediaRestClient(RestTemplate rest) {
+        this.rest = rest;
+    }
 
-	@Override
-	public void deleteImpl(Media media) {
-		rest.delete(MediaEndpoints.DELETE, media.getId());
-	}
+    @Override
+    public void deleteImpl(Media media) {
+        rest.delete(MediaEndpoints.DELETE, media.getId());
+    }
 
-	@Override
-	public List<String> listIDs() {
-		return List.of(rest.getForObject(MediaEndpoints.LIST_IDS, String[].class));
-	}
+    @Override
+    public List<String> listIDs() {
+        return List.of(rest.getForObject(MediaEndpoints.LIST_IDS, String[].class));
+    }
 
-	@Override
-	public List<?> listAllMetadata() {
+    @Override
+    public List<?> listAllMetadata() {
 
-		return List.of(rest.getForObject(MediaEndpoints.LISTALL_METADATA, String[].class));
-	}
+        return List.of(rest.getForObject(MediaEndpoints.LISTALL_METADATA, String[].class));
+    }
 
-	@Override
-	public URL getURL(String id) {
-		try {
-			return new URL(String.format("%s%s%s", getPath(),  MediaEndpoints.GET , id));
-		} catch (MalformedURLException e) {
-			throw new MediaError(e);
-		}
-	}
+    @Override
+    public URL getURL(String id) {
+        try {
+            return new URL(String.format("%s%s%s", getPath(), MediaEndpoints.GET, id));
+        } catch (MalformedURLException e) {
+            throw new MediaError(e);
+        }
+    }
 
-	@Override
-	public boolean validateURL(URL url) throws MediaException {
-		return false;
-	}
+    @Override
+    public boolean validateURL(URL url) throws MediaException {
+        return false;
+    }
 
 
-	@Override
-	public String getServerChecksum(String id) {
-		return rest.getForObject(MediaEndpoints.CHECKSUM, String.class, id);
-	}
+    @Override
+    public String getServerChecksum(String id) {
+        return rest.getForObject(MediaEndpoints.CHECKSUM, String.class, id);
+    }
 
-	@Override
-	public Media uploadImpl(Media media) throws MediaException{
-		return rest.postForObject(MediaEndpoints.UPLOAD+"_impl", media, Media.class);
-	}
+    @Override
+    public Media uploadImpl(Media media) throws MediaException {
 
-	@Override
-	public Media get(String id) throws MediaException {
-		InputStream stream = rest.getForObject("/get/",InputStream.class, id);
-		if (stream == null) 
-			return null;
-		
-		return new Media(id, stream);
-		
-	}
+        BufferedReader streamReader;
+        StringBuilder responseStrBuilder = new StringBuilder();
+        try {
+            streamReader = new BufferedReader(new InputStreamReader(media.getStream(), "UTF-8"));
+            String inputStr;
+            while ((inputStr = streamReader.readLine()) != null) responseStrBuilder.append(inputStr);
 
-	@Override
-	public MediaTemplate setPath(String pathStr) {
-		this.path = pathStr;
-		rest.setUriTemplateHandler(new DefaultUriBuilderFactory(this.path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-		return this;
-	}
+        JSONObject mediaJson = new JSONObject(media);
+        mediaJson.put("stream", responseStrBuilder.toString());
+        return rest.postForObject(MediaEndpoints.UPLOAD + "_impl", mediaJson, Media.class);
+    }
 
-	@Override
-	public String getPath() {
-		return path;
-	}
+
+    @Override
+    public Media get(String id) throws MediaException {
+        InputStream stream = rest.getForObject("/get/", InputStream.class, id);
+        if (stream == null) return null;
+
+        return new Media(id, stream);
+
+    }
+
+    @Override
+    public MediaTemplate setPath(String pathStr) {
+        this.path = pathStr;
+        rest.setUriTemplateHandler(new DefaultUriBuilderFactory(this.path));
+
+        return this;
+    }
+
+    @Override
+    public String getPath() {
+        return path;
+    }
 
 }
